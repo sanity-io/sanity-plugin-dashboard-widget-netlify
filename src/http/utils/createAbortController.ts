32b45460d1f71c74dc4a40ed6af1d8a839b5423f
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2022-09-02 15:15:52 +0200

    fix: compile errors

--- src/http/utils/createAbortController.ts
+++ src/http/utils/createAbortController.ts
@@ -2,7 +2,7 @@ import AbortControllerPolyfill from 'abort-controller'
 
 export const createAbortController = (): AbortController => {
   if (!('AbortController' in window)) {
-    return new AbortControllerPolyfill()
+    return new AbortControllerPolyfill() as AbortController
   }
   return new AbortController()
 }
